77713cc0c02cd6a5f2b6ac18a0ec028529282641
palaych <palaychm@yandex.ru>
2021-07-06 05:10:11 +0900

    sdm845-common: Drop QTI Thermal HAL 2.0
     * Work not correct with sdm845
    
    This reverts commit 69d09d2878e99f8fd23059fa3dc088e21844161e.
    
    Change-Id: I551cce1e223e3ead8c14d8899e76bc489d4d6eb0

--- sdm845.mk
+++ sdm845.mk
@@ -476,7 +476,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti \
     android.hardware.thermal@1.0 \
     android.hardware.thermal@1.0.vendor \
     android.hardware.thermal@1.1 \
