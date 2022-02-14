d57ceecac1c12ed2979bbbc6a6721df774940ad4
Lostark13 <tushar9915@gmail.com>
2022-02-14 17:26:26 +0530

    sdm845-common: parts: Add MiSound scenes
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Change-Id: I04b09a70dca1cfc989c7b84d8e73fab838f47beb
    Signed-off-by: resist15 <souravhjagtap21@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -38,12 +38,14 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
+    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
     private DiracUtils mDiracUtils;
+    private ListPreference mScenes;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -67,6 +69,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
         mPreset.setEnabled(enhancerEnabled);
+
+        mHeadsetType = (ListPreference) findPreference(PREF_SCENE);
+        mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -78,6 +84,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
                 return true;
             case PREF_PRESET:
                 mDiracUtils.setLevel((String) newValue);
+                return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
                 return true;
             default:
                 return false;
@@ -93,5 +102,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
     }
 }
