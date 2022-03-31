da6ccc2f8b6c5d77bb70598db0fdfa9044d89cf7
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-31 20:37:52 +0530

    sdm845-common: Enable Call recording feature for GDialer
    Keeping it here to avoid enabling it globally.
    
    Change-Id: I102f5d425003e6a26489c87b5e80ee44c4a9c7b1
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# Google Dialer Call recording
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
