03757859285eaa89cf32dbdab06e7269da58ac1e
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    sdm845-common: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- sdm845.mk
+++ sdm845.mk
@@ -345,6 +345,10 @@ PRODUCT_COPY_FILES += \
 # msm_irqbalance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+    
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor:64
 
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
