ca32541229ec27339252339688299c76ad636c38
Chenyang Zhong <zhongcy95@gmail.com>
2022-10-26 14:37:26 +0530

    sdm845-common: Kill deprecated/unneeded board flags * MAX_EGL_CACHE_KEY_SIZE - Not used anywhere. * MAX_EGL_CACHE_SIZE - Not used anywhere.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,9 +68,6 @@ TARGET_USES_GRALLOC1 := true
 TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
