abb8d6fa8fad2c05afbb104f1a118ea82c412b15
sajidshahriar <sazidshahriar39@gmail.com>
2023-01-09 11:11:21 +0000

    sdm845-common: vibrator: Revert previous impl
    Revert "sdm845-common: vibrator: Remove ndk_platform backend. Use the ndk backend."
    
    This reverts commit 1044a8ad67261d34c93376ab57ad69c74910b69e.
    
    Revert "sdm845-common: Adapt vendor.qti.hardware.vibrator.impl for A12"
    
    This reverts commit a1099bd55ee8a2c3c12dc600dec8a3bd5dccf1ce.
    
    Revert "sdm845-common: vibrator: Adapt Effects for Xiaomi sdm845 Devices"
    
    This reverts commit de81638e5c43eef09bc316aeb897d4cca853d642.
    
    Revert "sdm845-common: vibrator: Implement OOS style effects for LED vibrator"
    
    This reverts commit d1e7db72fe12b2b375c3a55229824b9c16d0704c.
    
    Revert "sdm845-common: Import QTI vibrator HAL"
    
    This reverts commit 423eccfdc8ab76463a7869d62a967c02555b8041.
    
    Signed-off-by: sajidshahriar <sazidshahriar39@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -512,9 +512,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.weaver@1.0 \
     android.hardware.weaver@1.0.vendor
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_sdm845
 
 # GcamGO
 PRODUCT_PACKAGES += \
